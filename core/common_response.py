b9f7dbde74a6ec6298c0c771e5b98110e4e1ead2
@nguyentronghuan101120 <nguyentronghuan101120@gmail.com>
2023-11-22 21:30:12 +0700

    Update base response

--- core/common_response.py
+++ core/common_response.py
@@ -3,6 +3,12 @@ import typing
 
 class BaseResponseModel(BaseModel):
     statusCode: int = 200
-    message: str
-    data: typing.Any
+    message: typing.Optional[typing.AnyStr] = None
+    data: typing.Optional[typing.Any] = None 
 
+    def to_json(self) -> dict:
+        return {
+            "status":self.statusCode,
+            "message": self.message,
+            "data":self.data
+        }
--- main.py
+++ main.py
@@ -12,100 +12,101 @@ mydb = mysql.connector.connect(
     port=3307,
 )
 
-
 mycursor = mydb.cursor()
-
 
 @app.route("/get-users")
 def getUsers():
-    mycursor.execute("select * from users")
-    myresult = mycursor.fetchall()
-
-    return BaseResponseModel(data=jsonify(myresult))
-
-
-@app.route("/get-user")
-def getUserById():
-    data = request.get_json()
-
-    if not data:
-        return jsonify("Missing body"), 400
-    elif "id" not in data:
-        return jsonify("Missing id"), 400
+    try:
+        mycursor.execute("select * from users")
+        myresult = mycursor.fetchall()
+        return jsonify(BaseResponseModel(data=myresult).to_json()),200
     
-    id = data["id"]
-    mycursor.execute("select * from users where id = %s", (id,))
-    myresult = mycursor.fetchall()
-
-    if not myresult:
-        return jsonify("Not found data"), 200
-
-    else:
-        return jsonify(myresult), 200
+    except Exception as e:
+        return jsonify(BaseResponseModel(data=myresult).to_json()),400
 
 
-@app.route("/delete-user", methods=["DELETE"])
-def deteleUserById():
-    data = request.get_json()
+# @app.route("/get-user")
+# def getUserById():
+#     data = request.get_json()
 
-    if not data:
-        return jsonify("Missing body"), 400
-    elif "id" not in data:
-        return jsonify("Missing id"), 400
+#     if not data:
+#         return jsonify("Missing body"), 400
+#     elif "id" not in data:
+#         return jsonify("Missing id"), 400
     
-    id = data["id"]
+#     id = data["id"]
+#     mycursor.execute("select * from users where id = %s", (id,))
+#     myresult = mycursor.fetchall()
 
-    mycursor.execute("delete from users where id = %s", (id,))
-    myresult = mycursor.fetchall()
-    mydb.commit()
+#     if not myresult:
+#         return jsonify("Not found data"), 200
 
-    return jsonify(myresult), 200
-
-
-@app.route("/create-user", methods=["POST"])
-def createUser():
-    data = request.get_json()
-
-    # Check if the 'email' field exists in the JSON data
-    if not data:
-        return jsonify({"error": "Missing body"}), 400
-    elif "email" not in data:
-        return jsonify({"error": "Missing email field"}), 400
-    elif "name" not in data:
-        return jsonify({"error": "Missing name field"}), 400
-
-    name = data["name"]  # Access the 'name' field from the JSON data
-    email = data["email"]  # Access the 'email' field from the JSON data
-
-    mycursor.execute("insert into users (name, email) values (%s, %s)", (name, email))
-    mydb.commit()
-    return jsonify(data), 200
+#     else:
+#         return jsonify(myresult), 200
 
 
-@app.route("/update-user/<userId>", methods=["POST"])
-def updateUser(userId):
-    data = request.get_json()
+# @app.route("/delete-user", methods=["DELETE"])
+# def deteleUserById():
+#     data = request.get_json()
 
-    # Check if the 'email' field exists in the JSON data
-    if not data:
-        return jsonify({"error": "Missing body"}), 400
-    elif "email" not in data:
-        return jsonify({"error": "Missing email field"}), 400
-    elif "name" not in data:
-        return jsonify({"error": "Missing name field"}), 400
+#     if not data:
+#         return jsonify("Missing body"), 400
+#     elif "id" not in data:
+#         return jsonify("Missing id"), 400
+    
+#     id = data["id"]
 
-    name = data["name"]  # Access the 'name' field from the JSON data
-    email = data["email"]  # Access the 'email' field from the JSON data
+#     mycursor.execute("delete from users where id = %s", (id,))
+#     myresult = mycursor.fetchall()
+#     mydb.commit()
 
-    mycursor.execute(
-        "UPDATE users SET name = %s, email = %s WHERE id = %s",
-        (name, email, userId),
-    )
-    mydb.commit()
-    return jsonify(data), 200
+#     return jsonify(myresult), 200
 
-@app.route("/register", methods=["POST"])
-def register():
+
+# @app.route("/create-user", methods=["POST"])
+# def createUser():
+#     data = request.get_json()
+
+#     # Check if the 'email' field exists in the JSON data
+#     if not data:
+#         return jsonify({"error": "Missing body"}), 400
+#     elif "email" not in data:
+#         return jsonify({"error": "Missing email field"}), 400
+#     elif "name" not in data:
+#         return jsonify({"error": "Missing name field"}), 400
+
+#     name = data["name"]  # Access the 'name' field from the JSON data
+#     email = data["email"]  # Access the 'email' field from the JSON data
+
+#     mycursor.execute("insert into users (name, email) values (%s, %s)", (name, email))
+#     mydb.commit()
+#     return jsonify(data), 200
+
+
+# @app.route("/update-user/<userId>", methods=["POST"])
+# def updateUser(userId):
+#     data = request.get_json()
+
+#     # Check if the 'email' field exists in the JSON data
+#     if not data:
+#         return jsonify({"error": "Missing body"}), 400
+#     elif "email" not in data:
+#         return jsonify({"error": "Missing email field"}), 400
+#     elif "name" not in data:
+#         return jsonify({"error": "Missing name field"}), 400
+
+#     name = data["name"]  # Access the 'name' field from the JSON data
+#     email = data["email"]  # Access the 'email' field from the JSON data
+
+#     mycursor.execute(
+#         "UPDATE users SET name = %s, email = %s WHERE id = %s",
+#         (name, email, userId),
+#     )
+#     mydb.commit()
+#     return jsonify(data), 200
+
+# @app.route("/register", methods=["POST"])
+# def register():
     # data = request.get_json()
     
     # if not data:
